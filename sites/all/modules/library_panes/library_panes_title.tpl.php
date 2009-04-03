3ea70412062ff4f65ab26d064374f368ad21080a
Mikkel Hoegh <mikkel@hoegh.org>
2009-04-03 10:15:58 +0200

    New library panes module for panels content types.

---
+++ sites/all/modules/library_panes/library_panes_title.tpl.php
@@ -0,0 +1,11 @@
+<?php
+// $Id$
+
+/**
+ * @file library_panes_title.tpl.php
+ * The main library page title with navigation, etc.
+ */
+?>
+  <h1><?php print $node->title; ?></h1>
+  <?php print $library_navigation; ?>
+
