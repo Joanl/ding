542e10d4c193947ae99a2711e70d225818be4d1b
mortendk <mdk@bookroyale.local>
2009-04-24 00:36:58 +0200

    mere style tam tam

--- sites/all/themes/dynamo/panels-twocol-left-stacked.tpl.php
+++ sites/all/themes/dynamo/panels-twocol-left-stacked.tpl.php
@@ -1,49 +1,27 @@
-<?php
-// $Id$
-
-/**
- * @file panels_threecol_left_stacked.tpl.php
- * Template for a 3 column panel where the left column is stacked with 
- * top and bottom rows.
- *
- * Variables:
- * - $id: An optional CSS id to use for the layout.
- * - $content: An array of content, each item in the array is keyed to one
- *   panel of the layout. This layout supports the following sections:
- *   - $content['top']: Content in the top row.
- *   - $content['left']: Content in the left column.
- *   - $content['right']: Content in the right column.
- *   - $content['bottom']: Content in the bottom row.
- */
-?>
-<div class="panel-display panel-2col-left-stacked clear-block" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-<?php if (!empty($content['right'])): ?>
-  <div class="panel-col right">
-    <div class="inside"><?php print $content['right']; ?></div>
-  </div>
-<?php endif; ?>
-
-<?php if (!empty($content['top']) || !empty($content['left']) || !empty($content['bottom'])): ?>
-  <div class="left-wrapper">
+<div id="twocol-left">
 
   <?php if (!empty($content['top'])): ?>
-    <div class="panel-col top">
-      <div class="inside"><?php print $content['top']; ?></div>
+    <div id="twocol-left-top">
+      <?php print $content['top']; ?>
     </div>
   <?php endif; ?>
 
-  <?php if (!empty($content['left'])): ?>
-    <div class="panel-col left">
-      <div class="inside"><?php print $content['left']; ?></div>
-    </div>
+  <div id="twocol-left-left">
+    <?php print $content['left']; ?>      
+  </div>
+
+</div>
+
+<div id="twocol-right">
+
+  <?php if (!empty($content['right'])): ?>
+    <?php print $content['right']; ?>
   <?php endif; ?>
 
   <?php if (!empty($content['bottom'])): ?>
-    <div class="panel-col bottom">
-      <div class="inside"><?php print $content['bottom']; ?></div>
+    <div id="twocol-right-bottom">
+    <?php print $content['bottom']; ?>        
     </div>
   <?php endif; ?>
-  </div>
-</div>
-<?php endif; ?>
 
+</div>  
