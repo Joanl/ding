ae1466a45c695c36daa6378d6ec676e3bbe83611
Kasper Garnæs <kasper@garnaes.dk>
2009-04-24 13:53:57 +0200

    Show opening hours in sections Added separator between address and opening hours

--- sites/all/modules/ding_library_map/ding_library_map.tpl.php
+++ sites/all/modules/ding_library_map/ding_library_map.tpl.php
@@ -23,6 +23,7 @@ foreach ($nodes as $node)
 									'street' => $node->location['street'], 
 									'city' => $node->location['city'], 
 									'postal-code' => $node->location['postal_code'],
+									'opening_hours' => $node->field_opening_hours[0],
 									'state' => 'open', 
 									'url' => url('node/'.$node->nid),
 									'text' => FALSE);
@@ -39,8 +40,8 @@ foreach ($nodes as $node)
 				<div class="street"></div>
 				<div><span class="postal-code"></span> <span class="city"></span></div>		
 		  </div>
-		  <div class="office-hours">
-		  </div>
+		  <dl class="opening-hours">
+		  </dl>
 		</div>
 	</div>
 </div>
@@ -49,24 +50,80 @@ foreach ($nodes as $node)
 </a>
 
 <script type="text/javascript">
+	var fullDayNames = {	'mon': '<?php echo t('Monday') ?>',
+			 									'tue': '<?php echo t('Tuesday') ?>',
+												'wed': '<?php echo t('Wednesday') ?>',
+												'thu': '<?php echo t('Thursday') ?>',
+												'fri': '<?php echo t('Friday') ?>',
+												'sat': '<?php echo t('Saturday') ?>',
+												'sun': '<?php echo t('Sunday') ?>' };
+	var shortDayNames = {	'mon': '<?php echo t('Mon') ?>',
+ 		 										'tue': '<?php echo t('Tue') ?>',
+				 								'wed': '<?php echo t('Wed') ?>',
+				 								'thu': '<?php echo t('Thu') ?>',
+				 								'fri': '<?php echo t('Fri') ?>',
+				 								'sat': '<?php echo t('Sat') ?>',
+				 								'sun': '<?php echo t('Sun') ?>' };
+								
 	$(document).ready(function()
 	{
 		map = Drupal.gmap.getMap('<?php echo $mapId ?>');
 		map.bind('mouseovermarker', function(object)
 		{
-			$('#library-info').removeClass('open').removeClass('closed').addClass(object.state);
+			console.log(object);
+			//Add address attributes
 			attributes = ['name', 'street', 'postal-code', 'city'];
 			for (i in attributes)
 			{
 				$('#library-info .'+attributes[i]).text(object[attributes[i]]);
 			}
+			
+			//Add opening hours
+			section = $('#library-info .opening-hours').empty(); 
+			days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']; //collection of weekdays to use for iteration
+			day = 0; //current day index
+			while (day < days.length) //iterate until end of week
+			{
+				sectionDays = [days[day]]; //add current day to to section
+				startTime = object.opening_hours[days[day]+'_start'];
+				endTime = object.opening_hours[days[day]+'_end'];
+				
+				nextDay = day + 1;
+				while ((startTime == object.opening_hours[days[nextDay]+'_start']) &&
+								(endTime == object.opening_hours[days[nextDay]+'_end'])) {
+					sectionDays.push(days[nextDay]); //if following days have same hours as current day then add these days to section
+					nextDay++;
+				}
+				
+				if (sectionDays.length > 1) {
+					sectionDays = shortDayNames[sectionDays.shift()]+'-'+shortDayNames[sectionDays.pop()]; //use short day names if section spans more than one day
+				} else {
+					sectionDays = fullDayNames[sectionDays.shift()]; //use full day name for section spanning a single day
+				}
+				
+				//add section to opening hours container
+				section.append('<dt>'+sectionDays+'</dt>');
+				section.append(	'<dd>'+
+													startTime.substr(0, startTime.lastIndexOf(':'))+
+													' - '+
+													endTime.substr(0, endTime.lastIndexOf(':'))+
+												'</dd>');
+				
+				day = nextDay; //step to day after last day in current section
+			}
+			
+			$('#library-info').removeClass('open').removeClass('closed').addClass(object.state);
+			
+			//Position and show info
 			point = Drupal.gmap.getMap('<?php echo $mapId ?>').map.fromLatLngToContainerPixel(object.marker.getLatLng());
 			$('#library-info').css({ 'left': (point.x-5)+'px', 'top': (point.y- $('#library-info').outerHeight())+'px' }).show();						
 		});
+		
 		$('#library-info').bind('mouseleave', function()
 		{
 			$('#library-info').hide();
-		});		
+		});
+		
 		map.bind('click', function(object) {
 			//TODO: Remove default binding for click event
 			window.location = object.url;
