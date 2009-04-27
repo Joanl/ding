b4ec2639961ca39a229c5f306fc0d178c6887262
mortendk <mdk@bookroyale.local>
2009-04-27 14:45:39 +0200

    fixet logo header + calender  forsider

--- sites/all/themes/dynamo/views-view-fields--event-list--block.tpl.php
+++ sites/all/themes/dynamo/views-view-fields--event-list--block.tpl.php
@@ -8,7 +8,7 @@
   <div class="month"><?php print date("M", $date);?></div>
 </div>
 
-<div>
+<div class="data">
   <?php print $fields['field_library_ref_nid']->content; ?>
   <h4><?php print $fields['title']->content; ?></h4>
   <?php print date("h:i", $date); ?>  -   <?php print $fields['field_entry_price_value']->content; ?>
