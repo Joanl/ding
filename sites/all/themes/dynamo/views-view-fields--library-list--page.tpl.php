e10798f656bdf88c007813d6b0e1a71705b01e9e
mortendk <mdk@bookroyale.local>
2009-04-29 16:19:35 +0200

    mere biblo

--- sites/all/themes/dynamo/views-view-fields--library-list--page.tpl.php
+++ sites/all/themes/dynamo/views-view-fields--library-list--page.tpl.php
@@ -3,8 +3,11 @@
     <?php print $fields['field_image_fid']->content; ?>  
   </div>
   <div class="info">
+    <?php $fields['field_opening_hours_processed']->status;?>
+
     <div class="libary-status open">open</div>
     <div class="libary-status close">close</div>    
+    
     <h3><?php print $fields['title']->content; ?></h3>
     <?php print $fields['address']->content;?>
   </div>
