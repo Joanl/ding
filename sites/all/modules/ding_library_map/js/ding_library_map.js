72d8dc47922cc5e6f1ecc52025e17f21f0026609
Kasper Garnæs <kasper@garnaes.dk>
2009-04-30 14:02:05 +0200

    More fixes for Ding Library map

--- sites/all/modules/ding_library_map/js/ding_library_map.js
+++ sites/all/modules/ding_library_map/js/ding_library_map.js
@@ -77,7 +77,8 @@ Drupal.dingLibraryMap = function(mapId, options)
 				sectionDays = [days[day]]; //add current day to to section
 				nextDay = day + 1;
 
-				if (object.opening_hours[[days[day]]])
+				if (object.opening_hours[[days[day]]] &&
+						object.opening_hours[[days[day]]].length > 0)
 				{
 					startTime = object.opening_hours[[days[day]]][0].start;
 					endTime = object.opening_hours[[days[day]]][0].end;
